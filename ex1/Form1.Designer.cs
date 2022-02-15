e66172d7a792200d93d381670c151fa5d501d0b5
joao victor pereira soares <joaosores4@gmail.com>
2022-02-15 11:13:26 -0300

    Initial commit

---
+++ ex1/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace ex1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btsoma = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.valor1 = new System.Windows.Forms.TextBox();
+            this.valor2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.resultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btsoma
+            // 
+            this.btsoma.Location = new System.Drawing.Point(112, 250);
+            this.btsoma.Name = "btsoma";
+            this.btsoma.Size = new System.Drawing.Size(75, 23);
+            this.btsoma.TabIndex = 0;
+            this.btsoma.Text = "soma";
+            this.btsoma.UseVisualStyleBackColor = true;
+            this.btsoma.Click += new System.EventHandler(this.btsoma_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "valor1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "valor2";
+            // 
+            // valor1
+            // 
+            this.valor1.Location = new System.Drawing.Point(129, 62);
+            this.valor1.Name = "valor1";
+            this.valor1.Size = new System.Drawing.Size(176, 20);
+            this.valor1.TabIndex = 3;
+            // 
+            // valor2
+            // 
+            this.valor2.Location = new System.Drawing.Point(129, 135);
+            this.valor2.Name = "valor2";
+            this.valor2.Size = new System.Drawing.Size(176, 20);
+            this.valor2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 381);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "resultado:";
+            // 
+            // resultado
+            // 
+            this.resultado.AutoSize = true;
+            this.resultado.Location = new System.Drawing.Point(123, 381);
+            this.resultado.Name = "resultado";
+            this.resultado.Size = new System.Drawing.Size(0, 13);
+            this.resultado.TabIndex = 6;
+            this.resultado.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(906, 512);
+            this.Controls.Add(this.resultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.valor2);
+            this.Controls.Add(this.valor1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btsoma);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btsoma;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox valor1;
+        private System.Windows.Forms.TextBox valor2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label resultado;
+    }
+}
+
---
+++ ex1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ex1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btsoma_Click(object sender, EventArgs e)
+        {
+            int  v1, v2, btsoma;
+            v1 = int.Parse(valor1.Text);
+            v2 = int.Parse(valor2.Text);
+            btsoma = valor1 + valor2;
+            resultado.Text = btsoma.ToString();
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
